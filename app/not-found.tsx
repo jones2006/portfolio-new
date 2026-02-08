6818878b2f25a5d74942dca4267278b4d413dad0
jones2006 <jones.j8637@gmail.com>
2026-02-08 21:33:49 +0530

    not-found-page

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center text-center bg-white text-black px-6 gap-2">
+      <h1 className="lg:text-8xl font-bold mb-4 text-4xl">Error-404❌</h1>
+
+      <p className="lg:text-2xl text-lg mb-6">Sorry mapla 😅 this page doesn’t exist.</p>
+
+      <Link
+        href="/"
+        className="bg-[#F2E961] border-b-4 border-r-2 border-black px-8 py-3 rounded-full font-bold"
+      >
+        Go Home 😎
+      </Link>
+    </div>
+  );
+}
